8f91a3eb63a59006b25f15aa89236f9ebca33e94
Gaurav Mishra <mishra.gaurav@siemens.com>
2021-04-28 20:41:22 +0530

    chore(code-quality): Improve tests
    Signed-off-by: Gaurav Mishra <mishra.gaurav@siemens.com>

--- nirjas/languages/tests/test_javascript.py
+++ nirjas/languages/tests/test_javascript.py
@@ -1,33 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+SPDX-License-Identifier: LGPL-2.1
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+'''
+
 import unittest
 import os
 from nirjas.languages import javascript
-from nirjas.binder import readSingleLine, readMultiLineDiff
+from nirjas.binder import readSingleLine, readMultiLineDiff, contSingleLines
 
 
 class JSTest(unittest.TestCase):
-    testfile = os.path.join(os.path.abspath(os.path.dirname(__file__)), "TestFiles/textcomment.js")
+    '''
+    Test cases for JavaScript language.
+    :ivar testfile: Location of test file
+    '''
+    testfile = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                            "TestFiles/textcomment.js")
 
     def test_output(self):
+        '''
+        Check for the scan correctness.
+        '''
         regex = r'''(?<![pst]:)\/\/\s*(.*)'''
-        self.syntax_start = "/*"
-        self.syntax_end = '*/'
-        sign = '//'
-        comment_single = javascript.readSingleLine(self.testfile, regex, sign)
-        comment_multiline = javascript.readMultiLineDiff(self.testfile, self.syntax_start, self.syntax_end)
-        comment_contSingleline = javascript.contSingleLines(comment_single)
+        syntax_start = "/*"
+        syntax_end = '*/'
+        comment_single = readSingleLine(self.testfile, regex)
+        comment_multiline = readMultiLineDiff(self.testfile, syntax_start,
+                                              syntax_end)
+        comment_contSingleline = contSingleLines(comment_single)
         self.assertTrue(comment_single)
         self.assertTrue(comment_multiline)
         self.assertTrue(comment_contSingleline)
 
     def test_outputFormat(self):
+        '''
+        Check for the output format correctness.
+        '''
         regex = r'''(?<![pst]:)\/\/\s*(.*)'''
-        self.syntax_start = "/*"
-        self.syntax_end = '*/'
-        sign = '//'
+        syntax_start = "/*"
+        syntax_end = '*/'
         expected = javascript.javascriptExtractor(self.testfile).get_dict()
-        comment_single = readSingleLine(self.testfile, regex, sign)
-        comment_multiline = readMultiLineDiff(self.testfile, self.syntax_start, self.syntax_end)
-        comment_contSingleline = javascript.contSingleLines(comment_single)
+        comment_single = readSingleLine(self.testfile, regex)
+        comment_multiline = readMultiLineDiff(self.testfile, syntax_start,
+                                              syntax_end)
+        comment_contSingleline = contSingleLines(comment_single)
         file = self.testfile.split("/")
         output = {
         "metadata": {
@@ -51,19 +82,26 @@ class JSTest(unittest.TestCase):
                 output['single_line_comment'].append({"line_number":i[0], "comment": i[1]})
 
         if comment_contSingleline:
-            for idx, i in enumerate(comment_contSingleline[1]):
-                output['cont_single_line_comment'].append({"start_line": comment_contSingleline[1][idx], "end_line": comment_contSingleline[2][idx], "comment": comment_contSingleline[3][idx]})
+            for idx, _ in enumerate(comment_contSingleline[1]):
+                output['cont_single_line_comment'].append({
+                  "start_line": comment_contSingleline[1][idx],
+                  "end_line": comment_contSingleline[2][idx],
+                  "comment": comment_contSingleline[3][idx]})
 
         if comment_multiline:
-            try:
-                for idx, i in enumerate(comment_multiline[0]):
-                    output['multi_line_comment'].append({"start_line": comment_multiline[0][idx], "end_line": comment_multiline[1][idx], "comment": comment_multiline[2][idx]})
-            except:
-                pass
+            for idx, _ in enumerate(comment_multiline[0]):
+                output['multi_line_comment'].append({
+                  "start_line": comment_multiline[0][idx],
+                  "end_line": comment_multiline[1][idx],
+                  "comment": comment_multiline[2][idx]})
 
         self.assertEqual(output, expected)
 
     def test_Source(self):
+        '''
+        Test the source code extraction.
+        Call the source function and check if new file exists.
+        '''
         name = "source.txt"
         newfile = javascript.javascriptSource(self.testfile, name)
 
