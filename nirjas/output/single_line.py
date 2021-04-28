45378fecd033ae74fc1527a11a2afb3c052cf357
Gaurav Mishra <mishra.gaurav@siemens.com>
2021-04-28 20:37:33 +0530

    chore(code-quality): Improve output module
    Signed-off-by: Gaurav Mishra <mishra.gaurav@siemens.com>

--- nirjas/output/single_line.py
+++ nirjas/output/single_line.py
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 '''
 
 
-class SingleLine(object):
+class SingleLine:
     '''
     Store result for single line comments
     '''
